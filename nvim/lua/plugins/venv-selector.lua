810531410fc5458b0faa214933a915a370a9f767
Lydien SANDANASAMY <lydien@MacBook-Air-de-Lydien.local>
2025-04-20 11:10:44 +0200

    change keymap

--- nvim/lua/plugins/venv-selector.lua
+++ nvim/lua/plugins/venv-selector.lua
@@ -9,7 +9,7 @@ return {
   lazy = false,
   branch = 'regexp', -- This is the regexp branch, use this for the new version
   keys = {
-    { ',v', '<cmd>VenvSelect<cr>' },
+    { '<leader>,', '<cmd>VenvSelect<cr>' },
   },
   ---@type venv-selector.Config
   opts = {
