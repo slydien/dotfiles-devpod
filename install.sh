5146e2ab205f7380b328c01a9fa4b6fbc25097bc
Lydien SANDANASAMY <lydien@MacBook-Air-de-Lydien.local>
2025-04-26 19:44:07 +0200

    add to path

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ export XDG_CONFIG_HOME="$HOME/.config"
 mkdir -p "$XDG_CONFIG_HOME"
 
 ln -sf "$PWD/zsh-nix-shell" "$XDG_CONFIG_HOME/zsh-nix-shell"
+ln -sf "$PWD/nix-zsh-completions" "$XDG_CONFIG_HOME/nix-zsh-completions"
 
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME/nvim"
 
