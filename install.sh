d6d79e6ab3706329ac4ad6d27083b0327d621b37
Lydien SANDANASAMY <lydien@MacBook-Air-de-Lydien.local>
2025-04-26 19:09:52 +0200

    remove nix from here

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 export XDG_CONFIG_HOME="$HOME/.config"
 mkdir -p "$XDG_CONFIG_HOME"
-mkdir -p "$XDG_CONFIG_HOME/nixpkgs"
 
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME/nvim"
 
@@ -11,7 +10,3 @@ ln -sf "$PWD/.bash_aliases" "$HOME/.bash_aliases"
 ln -sf "$PWD/.zshrc" "$HOME/.zshrc"
 ln -sf "$PWD/.zsh_aliases" "$HOME/.zsh_aliases"
 ln -sf "$PWD/.tmux.conf" "$HOME/.tmux.conf"
-ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME/nixpkgs/config.nix"
-
-# install nix packages from config.nix
-nix-env -iA nixpkgs.myPackages
